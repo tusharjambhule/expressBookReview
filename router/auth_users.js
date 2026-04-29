73762a9cc04053c056a2e27d3989e64ab6a34c6b
TUSHAR JAMBHULE <tusharjambhule1986@gmail.com>
2026-04-29 22:23:01 +0530

    Updated project for grading

--- router/auth_users.js
+++ router/auth_users.js
@@ -9,8 +9,18 @@ const books = require("./general").books;
 
 // Register
 regd_users.post("/register", (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Unable to register user" });
+  }
+
+  const existingUser = users.find((user) => user.username === username);
+
+  if (existingUser) {
+    return res.status(409).json({ message: "User already exists!" });
+  }
 
   users.push({ username, password });
 
@@ -21,9 +31,18 @@ regd_users.post("/register", (req, res) => {
 
 // Login
 regd_users.post("/login", (req, res) => {
-  let username = req.body.username;
+  const username = req.body.username;
+  const password = req.body.password;
 
-  let token = jwt.sign({ username }, "access", {
+  const validUser = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (!validUser) {
+    return res.status(401).json({ message: "Invalid Login. Check username and password" });
+  }
+
+  const token = jwt.sign({ username }, "access", {
     expiresIn: "1h",
   });
 
@@ -33,28 +52,46 @@ regd_users.post("/login", (req, res) => {
   };
 
   return res.status(200).json({
-    message: "User successfully logged in",
+    message: "Login successful!",
   });
 });
 
-// Add Review
+// Add / Modify Review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  let isbn = req.params.isbn;
-  let review = req.query.review;
-  let username = req.session.authorization.username;
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+
+  if (!req.session.authorization) {
+    return res.status(401).json({ message: "User not logged in" });
+  }
+
+  const username = req.session.authorization.username;
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
 
   books[isbn].reviews[username] = review;
 
   return res.status(200).json({
-    message: "Review added successfully",
+    message: "Review added/updated successfully",
     reviews: books[isbn].reviews,
   });
 });
 
 // Delete Review
 regd_users.delete("/auth/review/:isbn", (req, res) => {
-  let isbn = req.params.isbn;
-  let username = req.session.authorization.username;
+  const isbn = req.params.isbn;
+
+  if (!req.session.authorization) {
+    return res.status(401).json({ message: "User not logged in" });
+  }
+
+  const username = req.session.authorization.username;
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
 
   delete books[isbn].reviews[username];
 
--- router/general.js
+++ router/general.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const axios = require("axios");
+
 const public_users = express.Router();
 
 let books = {
   "1": {
     author: "Chinua Achebe",
     title: "Things Fall Apart",
-    reviews: {
-      user1: "Excellent book"
-    }
+    reviews: {}
   },
   "2": {
     author: "Hans Christian Andersen",
@@ -18,46 +18,114 @@ let books = {
     author: "Dante Alighieri",
     title: "The Divine Comedy",
     reviews: {}
+  },
+  "4": {
+    author: "Jane Austen",
+    title: "Pride and Prejudice",
+    reviews: {}
+  },
+  "5": {
+    author: "Mark Twain",
+    title: "Adventures of Huckleberry Finn",
+    reviews: {}
+  },
+  "6": {
+    author: "Charles Dickens",
+    title: "Great Expectations",
+    reviews: {}
+  },
+  "7": {
+    author: "Leo Tolstoy",
+    title: "War and Peace",
+    reviews: {}
+  },
+  "8": {
+    author: "George Orwell",
+    title: "1984",
+    reviews: {}
+  },
+  "9": {
+    author: "Mary Shelley",
+    title: "Frankenstein",
+    reviews: {}
+  },
+  "10": {
+    author: "Homer",
+    title: "The Odyssey",
+    reviews: {}
   }
 };
 
 // Get all books
-public_users.get("/", function (req, res) {
+public_users.get("/", async function (req, res) {
   return res.status(200).json(books);
 });
 
-// Get by ISBN
+// Get book by ISBN using Promise + Axios
 public_users.get("/isbn/:isbn", function (req, res) {
-  let isbn = req.params.isbn;
-  return res.status(200).json(books[isbn]);
+  const isbn = req.params.isbn;
+
+  axios
+    .get("http://localhost:5000/")
+    .then((response) => {
+      if (response.data[isbn]) {
+        return res.status(200).json(response.data[isbn]);
+      } else {
+        return res.status(404).json({ message: "Book not found" });
+      }
+    })
+    .catch(() => {
+      return res.status(500).json({ message: "Error retrieving book" });
+    });
 });
 
-// Get by Author
-public_users.get("/author/:author", function (req, res) {
-  let author = req.params.author.toLowerCase();
+// Get books by Author using async/await + Axios
+public_users.get("/author/:author", async function (req, res) {
+  const author = req.params.author.toLowerCase();
 
-  let filtered = Object.values(books).filter(
-    (book) => book.author.toLowerCase() === author
-  );
+  try {
+    const response = await axios.get("http://localhost:5000/");
+    const result = Object.values(response.data).filter(
+      (book) => book.author.toLowerCase() === author
+    );
 
-  return res.status(200).json(filtered);
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({ message: "Error retrieving books" });
+  }
 });
 
-// Get by Title
-public_users.get("/title/:title", function (req, res) {
-  let title = req.params.title.toLowerCase();
+// Get books by Title using async/await + Axios
+public_users.get("/title/:title", async function (req, res) {
+  const title = req.params.title.toLowerCase();
 
-  let filtered = Object.values(books).filter(
-    (book) => book.title.toLowerCase() === title
-  );
+  try {
+    const response = await axios.get("http://localhost:5000/");
+    const result = Object.values(response.data).filter(
+      (book) => book.title.toLowerCase() === title
+    );
 
-  return res.status(200).json(filtered);
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({ message: "Error retrieving books" });
+  }
 });
 
-// Get Reviews
+// Get reviews
 public_users.get("/review/:isbn", function (req, res) {
-  let isbn = req.params.isbn;
-  return res.status(200).json(books[isbn].reviews);
+  const isbn = req.params.isbn;
+
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  const reviews = books[isbn].reviews;
+
+  if (Object.keys(reviews).length === 0) {
+    return res.status(200).json({ message: "No reviews found for this book" });
+  }
+
+  return res.status(200).json(reviews);
 });
 
 module.exports.general = public_users;
